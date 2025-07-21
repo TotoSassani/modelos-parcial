f299db37b5296efb3e96d597a5a7d814bcb2e50b
TotoSassani <tsassani@frba.utn.edu.ar>
2025-07-21 20:19:45 -0300

    Seguimos ejercicios

--- MundialBrasil2014.hs
+++ MundialBrasil2014.hs
@@ -1,20 +1,25 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Move filter" #-}
+import Data.Type.Equality (TestEquality)
+{-# HLINT ignore "Eta reduce" #-}
+
 data Jugador = Jugador{
     nombreJugador :: String,
     edad :: Float,
     promedioGol :: Float,
     habilidad :: Float,
     valorCansancio :: Float
-} deriving (Show, Eq)
+} deriving (Show, Eq,Ord)
 
 data Equipo = Equipo {
     nombreEquipo :: String,
     grupoAlQuePertenece :: Char,
     jugadoresDelEquipo :: [Jugador]
-} deriving (Eq,Show)
+} deriving (Eq,Show,Ord)
 
 
 
-martin = Jugador "Martin" 26 0 50 35 
+martin = Jugador "Martin" 26 0 50 35
 juan = Jugador "Juancho" 30 0.2 50 40
 maxi = Jugador "Maxi Lopez" 27 0.4 68 30
 jonathan = Jugador "Chueco" 20 1.5 80 99
@@ -29,3 +34,33 @@ losDeSiempre = Equipo "Los De Siempre" 'F' [jonathan, lean, brian]
 restoDelMundo = Equipo "Resto Del Mundo" 'A' [garcia, messi, aguero]
 
 --1)
+figura :: Equipo -> [Jugador]
+figura equipo = filter ((>75). habilidad). filter ((> 0) . promedioGol) $ jugadoresDelEquipo equipo
+
+habilidadJugador :: Equipo -> [Jugador]
+habilidadJugador equipo = filter ((> 75) . habilidad) (jugadoresDelEquipo equipo)
+
+promedioGolJugador :: [Jugador]-> [Jugador]
+promedioGolJugador  = filter ((> 0) . promedioGol)
+
+figura' :: Equipo -> [Jugador]
+figura' equipo =  promedioGolJugador . habilidadJugador $ equipo
+
+jugadoresFaranduleros = ["Maxi Lopez", "Icardi", "Aguero", "Caniggia", "Demichelis"]
+
+tieneFarandulero :: Equipo -> Bool
+tieneFarandulero equipo = any ((`elem` jugadoresFaranduleros) . nombreJugador) (jugadoresDelEquipo equipo)
+
+tieneFarandulero' :: Jugador -> Bool
+tieneFarandulero' = (`elem` jugadoresFaranduleros) . nombreJugador
+
+-- 3)
+figuritaDificil :: Char -> [Equipo] -> [Jugador]
+figuritaDificil letra equipo = filter (not . tieneFarandulero')  . filter ((< 27) . edad). concatMap figura . filter ((== letra). grupoAlQuePertenece) $ equipo
+
+esJugadorJoven :: Jugador -> Bool
+esJugadorJoven jugador = edad jugador < 27
+
+esFigurita ::Equipo -> Bool
+esFigurita equipo = null (figura equipo)
+
--- aQuemarEsasGrasitas.hs
+++ aQuemarEsasGrasitas.hs
@@ -1,15 +1,17 @@
 data Persona = Persona{
     edad :: Float,
     peso :: Float,
-    tonificacion :: Int
+    tonificacion :: Float
 } deriving (Eq, Show, Ord)
+
+type Rutina = Persona -> Persona
 
 pancho = Persona 40 120 1
 
 andres = Persona 22 80 6
 
 estaSaludable :: Persona -> Bool
-estaSaludable persona = tonificacion persona > 5 && not (estaObeso persona) 
+estaSaludable persona = tonificacion persona > 5 && not (estaObeso persona)
 
 estaObeso :: Persona -> Bool
 estaObeso persona = peso persona > 100
@@ -18,7 +20,65 @@ caloriasPorKilo :: Float
 caloriasPorKilo = 150
 
 bajarDePeso :: Persona -> Float -> Persona
-bajarDePeso persona calorias 
+bajarDePeso persona calorias
     |estaObeso persona = persona {peso = peso persona - (calorias / caloriasPorKilo )}
     |not (estaObeso persona) && edad persona > 30 && calorias > 200 = persona{peso = peso persona - 1}
     |otherwise = persona{peso=peso persona - (calorias / (peso persona * edad persona))}
+
+caminata ::  Float -> Float -> Rutina
+caminata _ _ presona = presona{peso = peso presona - 5}
+
+entrenamientoCinta :: Float->Float -> Rutina
+entrenamientoCinta _ minutos persona = persona{peso = peso persona-6 }-- mod minutos 5}
+
+pesas :: Float -> Float -> Rutina
+pesas pesoALevantar tiempo persona
+    |tiempo > 10 = persona{ tonificacion = tonificacion persona + pesoALevantar * 0.1}
+    |otherwise = persona
+
+colina :: Float->Float -> Rutina
+colina inclinacion minutos persona = persona{ peso = peso persona - 2 * minutos * inclinacion}
+
+montania :: Float-> Float -> Rutina
+montania inclinacion minutos = tonificar . colina (3*inclinacion) (minutos / 2) . colina inclinacion (minutos / 2)
+
+tonificar :: Rutina
+tonificar persona = persona{tonificacion = tonificacion persona + 1}
+
+
+hacerEjercicoConRecu :: Persona-> Float->Float ->[Rutina] -> Persona
+hacerEjercicoConRecu persona _ _ [] = persona
+hacerEjercicoConRecu persona resto minutos [ejercicio] = ejercicio resto minutos persona
+hacerEjercicoConRecu persona resto minutos (x:xs:xss) = x resto minutos persona : hacerEjercicoConRecu (xs: xss) resto minutos persona
+
+hacerEjercicoConFold :: [Rutina] -> Persona-> Float-> Float ->Persona
+hacerEjercicoConFold ejercicios persona resto minutos  = foldl (\p rutina -> rutina resto minutos p) persona ejercicios
+
+restarKilos :: Persona -> Persona -> Float
+restarKilos personaDespues personaAntes = peso personaAntes - peso personaDespues
+
+kilosPerdidos :: Persona ->Float->Float ->[Rutina] -> Float
+kilosPerdidos persona resto minutos ejercicios = restarKilos (hacerEjercicoConRecu persona resto minutos ejercicios) persona
+
+tonificacionResta :: Persona -> Persona -> Float
+tonificacionResta personaDespues personaAntes = tonificacion personaDespues - tonificacion personaAntes
+
+tonificacionGanado ::Persona ->Float->Float ->[Rutina] -> Float
+tonificacionGanado persona resto minutos ejercicios = tonificacionResta (hacerEjercicoConRecu persona resto minutos ejercicios) persona
+
+--Loma Montero
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
